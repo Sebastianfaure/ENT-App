49673e3f3de13047c4a146c24d401fdec1379e0f
Sebastianfaure <54461558+Sebastianfaure@users.noreply.github.com>
2021-04-05 16:11:41 -0400

    Add files via upload

---
+++ Laryngectomy.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ENT_APP
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Laryngectomy : ContentPage
+    {
+        public Laryngectomy()
+        {
+            InitializeComponent();
+        }
+
+        private async void Larynx(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
