d3912517a4cae1e87452e2b4ecc1edfb4c5dbb5c
dhrubo28 <58522980+dhrubo28@users.noreply.github.com>
2021-04-05 14:00:08 -0400

    Add files via upload

---
+++ Page1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2
+{
+    // [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private async void NavigateButton_OnClicked(object sender, EventArgs e)
+        {
+           // await Navigation.PushAsync(new MainPage());
+        }
+
+        async void MeettheTeamButtonClickedAsync(object sender, EventArgs e)
+        {
+            /*  private async void NavigateButton_OnClicked(object sender, EventArgs e) {  
+            await Navigation.PushAsync(new SecondPage());  
+        }  */
+            //   (sender as Button).Text = "Click me again!";
+            // await Navigation.PushAsync(new HomePage());
+        }
+
+        private async void TotalLaryngectomy_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page2()); 
+        }
+    }
+}
