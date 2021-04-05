fd1dee96e23c6580b4abccf349ad396dbc7d775c
Sebastianfaure <54461558+Sebastianfaure@users.noreply.github.com>
2021-04-05 16:14:08 -0400

    Add files via upload

---
+++ MySurgery.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ENT_APP
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MySurgery : ContentPage
+    {
+        public MySurgery()
+        {
+            InitializeComponent();
+        }
+        private async void NavigateButton_OnClicked(object sender, EventArgs e)
+        {
+            // await Navigation.PushAsync(new MainPage());
+        }
+
+        async void MeettheTeamButtonClickedAsync(object sender, EventArgs e)
+        {
+            /*  private async void NavigateButton_OnClicked(object sender, EventArgs e) {  
+            await Navigation.PushAsync(new SecondPage());  
+        }  */
+            //   (sender as Button).Text = "Click me again!";
+            // await Navigation.PushAsync(new HomePage());
+        }
+
+        private async void TotalLaryngectomy_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Laryngectomy());
+        }
+    }
+}
