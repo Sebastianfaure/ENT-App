b9d41adbbd22a417fa7610403f358586c8a3595a
Sebastianfaure <54461558+Sebastianfaure@users.noreply.github.com>
2021-04-12 16:23:23 -0400

    Update MeetTheTeam.xaml.cs

--- MeetTheTeam.xaml.cs
+++ MeetTheTeam.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +12,41 @@ namespace ENT_APP
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MeetTheTeam : ContentPage
     {
+        public IList<Doctor> listOfDoctors { get; set; }
         public MeetTheTeam()
         {
             InitializeComponent();
+            listOfDoctors = new List<Doctor>();
+
+            listOfDoctors.Add(new Doctor { 
+                DoctorName = "Peter T Dziegielewski, MD", 
+                Description = "Associate Professor & Chief, Division Of Head And Neck Oncologic And Micro-vascular Reconstructive Surgery", 
+                ImageURL = "PeterTDziegielewski.JPEG" 
+            });
+            listOfDoctors.Add(new Doctor { 
+                DoctorName = "Natalie L Silver, MD", 
+                Description = "Assistant Professor", 
+                ImageURL = "NatalieLSilver.JPEG"
+            });
+            listOfDoctors.Add(new Doctor { 
+                DoctorName = "Deepa Danan, MD", 
+                Description = "Assistant Professor, Head And Neck Oncology And Microvascular Reconstructive Surgery, Medical Director - Shands ENT Clinic", 
+                ImageURL = "DeepaDanan.JPEG"
+            });
+            listOfDoctors.Add(new Doctor { 
+                DoctorName = "Brian B Hughley, MD", 
+                Description = "Assistant Professor", 
+                ImageURL = "BrianBHughley.JPEG"
+            });
+
+            BindingContext = this;
         }
+    }
+
+    public class Doctor
+    {
+        public string DoctorName { get; set; }
+        public string Description { get; set; }
+        public string ImageURL { get; set; }
     }
 }
