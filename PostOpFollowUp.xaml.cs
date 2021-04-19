d199d086a7e6fdbeb354f67f3ab352cb2275b719
Sheno-Nicolas <58519253+Sheno-Nicolas@users.noreply.github.com>
2021-04-19 17:47:01 -0400

    Update PostOpFollowUp.xaml.cs

--- PostOpFollowUp.xaml.cs
+++ PostOpFollowUp.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +15,11 @@ namespace ENT_APP
         public PostOpFollowUp()
         {
             InitializeComponent();
+        }
+
+        private async void MySurgery_ClickedAsync(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MySurgery());
         }
     }
 }
