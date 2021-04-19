dddbbdd2b7c964e1423fa90f553d076d56a392ff
Sebastianfaure <54461558+Sebastianfaure@users.noreply.github.com>
2021-04-19 17:12:08 -0400

    Create quiz1.xaml.cs

---
+++ quiz1.xaml.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ENT_APP
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class quiz1 : ContentPage
+    {
+        public quiz1()
+        {
+            InitializeComponent();
+            webView.Source = "https://docs.google.com/forms/d/e/1FAIpQLScLHQ3bJudy05dVGHBf8LKyZRpxT0m4rwyzI1o5krDoS42Ogg/viewform";
+        }
+    }
+}
