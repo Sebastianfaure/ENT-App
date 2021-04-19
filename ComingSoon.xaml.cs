465977a68cb885ad92c490db9936d2503c5d0d2d
Sheno-Nicolas <58519253+Sheno-Nicolas@users.noreply.github.com>
2021-04-19 18:57:34 -0400

    Create ComingSoon.xaml.cs

---
+++ ComingSoon.xaml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ENT_APP
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ComingSoon : ContentPage
+    {
+        public ComingSoon()
+        {
+            InitializeComponent();
+        }
+    }
+}
