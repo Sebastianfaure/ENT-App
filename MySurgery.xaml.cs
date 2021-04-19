79ac95af7dd8e223a42831d63f9492d815026aa0
Sheno-Nicolas <58519253+Sheno-Nicolas@users.noreply.github.com>
2021-04-19 18:58:13 -0400

    Update MySurgery.xaml.cs

--- MySurgery.xaml.cs
+++ MySurgery.xaml.cs
@@ -16,23 +16,58 @@ namespace ENT_APP
         {
             InitializeComponent();
         }
-        private async void NavigateButton_OnClicked(object sender, EventArgs e)
-        {
+        //private async void NavigateButton_OnClicked(object sender, EventArgs e)
+        //{
             // await Navigation.PushAsync(new MainPage());
-        }
+        //}
 
-        async void MeettheTeamButtonClickedAsync(object sender, EventArgs e)
-        {
+        //async void MeettheTeamButtonClickedAsync(object sender, EventArgs e)
+        //{
             /*  private async void NavigateButton_OnClicked(object sender, EventArgs e) {  
             await Navigation.PushAsync(new SecondPage());  
         }  */
             //   (sender as Button).Text = "Click me again!";
             // await Navigation.PushAsync(new HomePage());
-        }
+        //}
 
         private async void TotalLaryngectomy_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new Laryngectomy());
+        }
+
+        private async void FeedingTube_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ComingSoon());
+        }
+
+        private async void NeckDissection_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ComingSoon());
+        }
+
+        private async void Tracheostomy_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ComingSoon());
+        }
+
+        private async void TORS_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ComingSoon());
+        }
+
+        private async void Maxillectom_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ComingSoon());
+        }
+
+        private async void MandibleResection_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ComingSoon());
+        }
+
+        private async void FreeFlapReconstruction_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ComingSoon());
         }
     }
 }
