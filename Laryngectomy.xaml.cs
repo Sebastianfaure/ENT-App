c2eaec0b42066fbc306b04d17dc465ae926fc8fa
Sebastianfaure <54461558+Sebastianfaure@users.noreply.github.com>
2021-04-20 20:04:19 -0400

    Update Laryngectomy.xaml.cs

--- Laryngectomy.xaml.cs
+++ Laryngectomy.xaml.cs
@@ -12,14 +12,26 @@ namespace ENT_APP
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Laryngectomy : ContentPage
     {
+        public IList<ExpandableDescription> listOfExpandableDescriptions { get; set; }
+        
+
         public Laryngectomy()
         {
             InitializeComponent();
-        }
+            listOfExpandableDescriptions = new List<ExpandableDescription>();
 
-        private async void Larynx(object sender, EventArgs e)
-        {
+            listOfExpandableDescriptions.Add(new ExpandableDescription
+            {
+                Title = "What is the larynx?",
+                Description = "The larynx (LA-ER-INKS) is the medical name for your voice box. Your larynx is in the front of your neck and sits behind the cartilage known as the adam's apple, on top of your windpipe (trachea), in front of your food pipe (esophagus), and below your throat (pharynx). Your larynx allows you to speak, breathe, and swallow normally. It is made up of different types of tissues including cartilage, muscles, and ligaments. These all work together to allow you to make different sounds, breathe, and protect your airway when swallowing."
+            });
+            listOfExpandableDescriptions.Add(new ExpandableDescription
+            {
+                Title = "",
+                Description = "",
+            });
 
+            BindingContext = this;
         }
 
         private async void quiz1_ClickedAsync(object sender, EventArgs e)
@@ -32,4 +44,10 @@ namespace ENT_APP
             await Navigation.PushAsync(new quiz2());
         }
     }
+}
+
+public class ExpandableDescription
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
 }
